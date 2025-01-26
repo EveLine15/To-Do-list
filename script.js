314b55797acfc7f3020d9a5d008a90722f613124
EveLine15 <nnasta788@gmail.com>
2025-01-26 17:42:09 +0300

    commit

--- script.js
+++ script.js
@@ -2,27 +2,55 @@ const namefield = document.getElementById('name');
 const textholder = document.getElementById('textholder');
 const statusPosition = document.getElementById('status');
 const addBtn = document.getElementById('add-task');
-const deleteBtn = document.getElementById('delete-task');
 
 const ul = document.querySelector('ul');
 
 let taskArray = [];
 
-addBtn.addEventListener('click', (event) => {
+addBtn.addEventListener('click', changeLayout);
+
+
+function changeLayout(event){
     event.preventDefault();
-    taskArray.push({name: namefield.value, discription: textholder.value, status: statusPosition.value});
-    const li = `<li>
-                        <div class="text-part">
-                            <p>${namefield.value}</p>
-                            <p>${textholder.value}</p>
-                            <p class="status-text">${statusPosition.value}</p>
+    ul.innerHTML = `<li>
+                        <div class="lable-li text-part">
+                            <p>Name</p>
+                            <p>Discription</p>
+                            <p>Status</p>
                         </div>
-                        <button>Delete</button>
                     </li>`;
-    ul.innerHTML += li;
-});
+    taskArray.push({number: taskArray.length, name: namefield.value, discription: textholder.value, status: statusPosition.value});
+    console.log(taskArray);
+    taskArray.forEach(element => {
+        createElement(element.number);
+        addDeleteOnBtns();
+    });
+    
+}
 
-deleteBtn.addEventListener('click', (event) => {
-    event.preventDefault();
+function createElement(numberOfElement){
+    const li = `<li id="${numberOfElement}-li">
+    <div class="text-part">
+        <p>${taskArray[numberOfElement].name}</p>
+        <p>${taskArray[numberOfElement].discription}</p>
+        <p class="status-text">${taskArray[numberOfElement].status}</p>
+    </div>
+    <button id="${numberOfElement}" class="delete-task">Delete</button>
+    </li>`;
+    ul.innerHTML += li; 
+}
 
-})
+function addDeleteOnBtns(){
+    const deleteBtns = document.querySelectorAll('.delete-task');
+    deleteBtns.forEach(deleteBtn => {
+        deleteBtn.addEventListener('click', (event) => {
+            event.preventDefault();
+            const li = document.getElementById(`${deleteBtn.id}-li`);
+            ul.removeChild(li);
+            console.log("deleteBtn.id: "+deleteBtn.id);
+            console.log(taskArray.findIndex(element => element.number == deleteBtn.id));
+            taskArray.splice(taskArray.findIndex(element => element.number == deleteBtn.id), 1);   
+            console.log(taskArray);
+        });
+    }); 
+}
